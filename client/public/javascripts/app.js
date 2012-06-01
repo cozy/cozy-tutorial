698387859b4e6e7d2c53b7761d61593121564304
Frank Rousseau <frank@mycozycloud.com>
2012-06-01 20:12:11 +0200

    debug entry point

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -1,25 +1,12 @@
 (function(/*! Brunch !*/) {
+  'use strict';
+
   if (!this.require) {
-    var modules = {}, cache = {}, require = function(name, root) {
-      var module = cache[name], path = expand(root, name), fn;
-      if (module) {
-        return module;
-      } else if (fn = modules[path] || modules[path = expand(path, './index')]) {
-        module = {id: name, exports: {}};
-        try {
-          cache[name] = module.exports;
-          fn(module.exports, function(name) {
-            return require(name, dirname(path));
-          }, module);
-          return cache[name] = module.exports;
-        } catch (err) {
-          delete cache[name];
-          throw err;
-        }
-      } else {
-        throw 'module \'' + name + '\' not found';
-      }
-    }, expand = function(root, name) {
+    var modules = {};
+    var cache = {};
+    var __hasProp = ({}).hasOwnProperty;
+
+    var expand = function(root, name) {
       var results = [], parts, part;
       if (/^\.\.?(\/|$)/.test(name)) {
         parts = [root, name].join('/').split('/');
@@ -35,19 +22,89 @@
         }
       }
       return results.join('/');
-    }, dirname = function(path) {
+    };
+
+    var getFullPath = function(path, fromCache) {
+      var store = fromCache ? cache : modules;
+      var dirIndex;
+      if (__hasProp.call(store, path)) return path;
+      dirIndex = expand(path, './index');
+      if (__hasProp.call(store, dirIndex)) return dirIndex;
+    };
+    
+    var cacheModule = function(name, path, contentFn) {
+      var module = {id: path, exports: {}};
+      try {
+        cache[path] = module.exports;
+        contentFn(module.exports, function(name) {
+          return require(name, dirname(path));
+        }, module);
+        cache[path] = module.exports;
+      } catch (err) {
+        delete cache[path];
+        throw err;
+      }
+      return cache[path];
+    };
+
+    var require = function(name, root) {
+      var path = expand(root, name);
+      var fullPath;
+
+      if (fullPath = getFullPath(path, true)) {
+        return cache[fullPath];
+      } else if (fullPath = getFullPath(path, false)) {
+        return cacheModule(name, fullPath, modules[fullPath]);
+      } else {
+        throw new Error("Cannot find module '" + name + "'");
+      }
+    };
+
+    var dirname = function(path) {
       return path.split('/').slice(0, -1).join('/');
     };
+
     this.require = function(name) {
       return require(name, '');
     };
+
     this.require.brunch = true;
     this.require.define = function(bundle) {
-      for (var key in bundle)
-        modules[key] = bundle[key];
+      for (var key in bundle) {
+        if (__hasProp.call(bundle, key)) {
+          modules[key] = bundle[key];
+        }
+      }
     };
   }
-}).call(this);(this.require.define({
+}).call(this);
+(this.require.define({
+  "helpers": function(exports, require, module) {
+    (function() {
+
+  exports.BrunchApplication = (function() {
+
+    function BrunchApplication() {
+      var _this = this;
+      $(function() {
+        _this.initialize(_this);
+        return Backbone.history.start();
+      });
+    }
+
+    BrunchApplication.prototype.initialize = function() {
+      return null;
+    };
+
+    return BrunchApplication;
+
+  })();
+
+}).call(this);
+
+  }
+}));
+(this.require.define({
   "initialize": function(exports, require, module) {
     (function() {
   var BrunchApplication, HomeView, MainRouter,
@@ -78,32 +135,6 @@
   })(BrunchApplication);
 
   window.app = new exports.Application;
-
-}).call(this);
-
-  }
-}));
-(this.require.define({
-  "helpers": function(exports, require, module) {
-    (function() {
-
-  exports.BrunchApplication = (function() {
-
-    function BrunchApplication() {
-      var _this = this;
-      $(function() {
-        _this.initialize(_this);
-        return Backbone.history.start();
-      });
-    }
-
-    BrunchApplication.prototype.initialize = function() {
-      return null;
-    };
-
-    return BrunchApplication;
-
-  })();
 
 }).call(this);
 
