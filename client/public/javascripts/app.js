31551ce9be15605af7c5b4c828e6b49e6606e6c3
Frank Rousseau <frank.rousseau@free.fr>
2012-08-22 20:31:09 +0200

    add main icon to cozy template

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -1,220 +1,211 @@
 (function(/*! Brunch !*/) {
   'use strict';
 
-  if (!this.require) {
-    var modules = {};
-    var cache = {};
-    var __hasProp = ({}).hasOwnProperty;
+  var globals = typeof window !== 'undefined' ? window : global;
+  if (typeof globals.require === 'function') return;
 
-    var expand = function(root, name) {
-      var results = [], parts, part;
-      if (/^\.\.?(\/|$)/.test(name)) {
-        parts = [root, name].join('/').split('/');
-      } else {
-        parts = name.split('/');
-      }
-      for (var i = 0, length = parts.length; i < length; i++) {
-        part = parts[i];
-        if (part == '..') {
-          results.pop();
-        } else if (part != '.' && part != '') {
-          results.push(part);
-        }
-      }
-      return results.join('/');
-    };
+  var modules = {};
+  var cache = {};
 
-    var getFullPath = function(path, fromCache) {
-      var store = fromCache ? cache : modules;
-      var dirIndex;
-      if (__hasProp.call(store, path)) return path;
-      dirIndex = expand(path, './index');
-      if (__hasProp.call(store, dirIndex)) return dirIndex;
-    };
-    
-    var cacheModule = function(name, path, contentFn) {
-      var module = {id: path, exports: {}};
-      try {
-        cache[path] = module.exports;
-        contentFn(module.exports, function(name) {
-          return require(name, dirname(path));
-        }, module);
-        cache[path] = module.exports;
-      } catch (err) {
-        delete cache[path];
-        throw err;
-      }
-      return cache[path];
-    };
+  var has = function(object, name) {
+    return hasOwnProperty.call(object, name);
+  };
 
-    var require = function(name, root) {
-      var path = expand(root, name);
-      var fullPath;
-
-      if (fullPath = getFullPath(path, true)) {
-        return cache[fullPath];
-      } else if (fullPath = getFullPath(path, false)) {
-        return cacheModule(name, fullPath, modules[fullPath]);
-      } else {
-        throw new Error("Cannot find module '" + name + "'");
-      }
-    };
-
-    var dirname = function(path) {
-      return path.split('/').slice(0, -1).join('/');
-    };
-
-    this.require = function(name) {
-      return require(name, '');
-    };
-
-    this.require.brunch = true;
-    this.require.define = function(bundle) {
-      for (var key in bundle) {
-        if (__hasProp.call(bundle, key)) {
-          modules[key] = bundle[key];
-        }
-      }
-    };
-  }
-}).call(this);
-(this.require.define({
-  "helpers": function(exports, require, module) {
-    (function() {
-
-  exports.BrunchApplication = (function() {
-
-    function BrunchApplication() {
-      var _this = this;
-      $(function() {
-        _this.initialize(_this);
-        return Backbone.history.start();
-      });
+  var expand = function(root, name) {
+    var results = [], parts, part;
+    if (/^\.\.?(\/|$)/.test(name)) {
+      parts = [root, name].join('/').split('/');
+    } else {
+      parts = name.split('/');
     }
-
-    BrunchApplication.prototype.initialize = function() {
-      return null;
-    };
-
-    return BrunchApplication;
-
-  })();
-
-}).call(this);
-
-  }
-}));
-(this.require.define({
-  "initialize": function(exports, require, module) {
-    (function() {
-  var BrunchApplication, HomeView, MainRouter,
-    __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
-
-  BrunchApplication = require('helpers').BrunchApplication;
-
-  MainRouter = require('routers/main_router').MainRouter;
-
-  HomeView = require('views/home_view').HomeView;
-
-  exports.Application = (function(_super) {
-
-    __extends(Application, _super);
-
-    function Application() {
-      Application.__super__.constructor.apply(this, arguments);
+    for (var i = 0, length = parts.length; i < length; i++) {
+      part = parts[i];
+      if (part === '..') {
+        results.pop();
+      } else if (part !== '.' && part !== '') {
+        results.push(part);
+      }
     }
+    return results.join('/');
+  };
 
-    Application.prototype.initialize = function() {
-      this.router = new MainRouter;
-      return this.homeView = new HomeView;
+  var dirname = function(path) {
+    return path.split('/').slice(0, -1).join('/');
+  };
+
+  var localRequire = function(path) {
+    return function(name) {
+      var dir = dirname(path);
+      var absolute = expand(dir, name);
+      return require(absolute);
     };
+  };
 
-    return Application;
+  var initModule = function(name, definition) {
+    var module = {id: name, exports: {}};
+    definition(module.exports, localRequire(name), module);
+    var exports = cache[name] = module.exports;
+    return exports;
+  };
 
-  })(BrunchApplication);
+  var require = function(name) {
+    var path = expand(name, '.');
 
-  window.app = new exports.Application;
+    if (has(cache, path)) return cache[path];
+    if (has(modules, path)) return initModule(path, modules[path]);
 
-}).call(this);
+    var dirIndex = expand(path, './index');
+    if (has(cache, dirIndex)) return cache[dirIndex];
+    if (has(modules, dirIndex)) return initModule(dirIndex, modules[dirIndex]);
 
-  }
-}));
-(this.require.define({
-  "routers/main_router": function(exports, require, module) {
-    (function() {
-  var __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+    throw new Error('Cannot find module "' + name + '"');
+  };
 
-  exports.MainRouter = (function(_super) {
-
-    __extends(MainRouter, _super);
-
-    function MainRouter() {
-      MainRouter.__super__.constructor.apply(this, arguments);
+  var define = function(bundle) {
+    for (var key in bundle) {
+      if (has(bundle, key)) {
+        modules[key] = bundle[key];
+      }
     }
-
-    MainRouter.prototype.routes = {
-      '': 'home'
-    };
-
-    MainRouter.prototype.home = function() {
-      return $('body').html(app.homeView.render().el);
-    };
-
-    return MainRouter;
-
-  })(Backbone.Router);
-
-}).call(this);
-
   }
-}));
-(this.require.define({
-  "views/home_view": function(exports, require, module) {
-    (function() {
-  var __hasProp = Object.prototype.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
-  exports.HomeView = (function(_super) {
+  globals.require = require;
+  globals.require.define = define;
+  globals.require.brunch = true;
+})();
 
-    __extends(HomeView, _super);
+window.require.define({"helpers": function(exports, require, module) {
+  (function() {
 
-    function HomeView() {
-      HomeView.__super__.constructor.apply(this, arguments);
-    }
+    exports.BrunchApplication = (function() {
 
-    HomeView.prototype.id = 'home-view';
+      function BrunchApplication() {
+        var _this = this;
+        $(function() {
+          _this.initialize(_this);
+          return Backbone.history.start();
+        });
+      }
 
-    HomeView.prototype.render = function() {
-      $(this.el).html(require('./templates/home'));
-      return this;
-    };
+      BrunchApplication.prototype.initialize = function() {
+        return null;
+      };
 
-    return HomeView;
+      return BrunchApplication;
 
-  })(Backbone.View);
+    })();
 
-}).call(this);
+  }).call(this);
+  
+}});
 
+window.require.define({"initialize": function(exports, require, module) {
+  (function() {
+    var BrunchApplication, HomeView, MainRouter,
+      __hasProp = Object.prototype.hasOwnProperty,
+      __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+    BrunchApplication = require('helpers').BrunchApplication;
+
+    MainRouter = require('routers/main_router').MainRouter;
+
+    HomeView = require('views/home_view').HomeView;
+
+    exports.Application = (function(_super) {
+
+      __extends(Application, _super);
+
+      function Application() {
+        Application.__super__.constructor.apply(this, arguments);
+      }
+
+      Application.prototype.initialize = function() {
+        this.router = new MainRouter;
+        return this.homeView = new HomeView;
+      };
+
+      return Application;
+
+    })(BrunchApplication);
+
+    window.app = new exports.Application;
+
+  }).call(this);
+  
+}});
+
+window.require.define({"routers/main_router": function(exports, require, module) {
+  (function() {
+    var __hasProp = Object.prototype.hasOwnProperty,
+      __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+    exports.MainRouter = (function(_super) {
+
+      __extends(MainRouter, _super);
+
+      function MainRouter() {
+        MainRouter.__super__.constructor.apply(this, arguments);
+      }
+
+      MainRouter.prototype.routes = {
+        '': 'home'
+      };
+
+      MainRouter.prototype.home = function() {
+        return $('body').html(app.homeView.render().el);
+      };
+
+      return MainRouter;
+
+    })(Backbone.Router);
+
+  }).call(this);
+  
+}});
+
+window.require.define({"views/home_view": function(exports, require, module) {
+  (function() {
+    var __hasProp = Object.prototype.hasOwnProperty,
+      __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+    exports.HomeView = (function(_super) {
+
+      __extends(HomeView, _super);
+
+      function HomeView() {
+        HomeView.__super__.constructor.apply(this, arguments);
+      }
+
+      HomeView.prototype.id = 'home-view';
+
+      HomeView.prototype.render = function() {
+        $(this.el).html(require('./templates/home'));
+        return this;
+      };
+
+      return HomeView;
+
+    })(Backbone.View);
+
+  }).call(this);
+  
+}});
+
+window.require.define({"views/templates/home": function(exports, require, module) {
+  module.exports = function anonymous(locals, attrs, escape, rethrow) {
+  var attrs = jade.attrs, escape = jade.escape, rethrow = jade.rethrow;
+  var buf = [];
+  with (locals || {}) {
+  var interp;
+  buf.push('<div');
+  buf.push(attrs({ 'id':('content') }));
+  buf.push('><h1>Cozy template</h1><h2>Welcome</h2><ul><li><a');
+  buf.push(attrs({ 'href':("https://github.com/mycozycloud/cozy-notes/wiki/Development-environment") }));
+  buf.push('>Documentation</a></li><li><a');
+  buf.push(attrs({ 'href':("https://github.com/mycozycloud") }));
+  buf.push('>Github</a></li></ul></div>');
   }
-}));
-(this.require.define({
-  "views/templates/home": function(exports, require, module) {
-    module.exports = function anonymous(locals, attrs, escape, rethrow) {
-var attrs = jade.attrs, escape = jade.escape, rethrow = jade.rethrow;
-var buf = [];
-with (locals || {}) {
-var interp;
-buf.push('<div');
-buf.push(attrs({ 'id':('content') }));
-buf.push('><h1>Cozy template</h1><h2>Welcome</h2><ul><li><a');
-buf.push(attrs({ 'href':("https://github.com/mycozycloud/cozy-notes/wiki/Development-environment") }));
-buf.push('>Documentation</a></li><li><a');
-buf.push(attrs({ 'href':("https://github.com/mycozycloud") }));
-buf.push('>Github</a></li></ul></div>');
-}
-return buf.join("");
-};
-  }
-}));
+  return buf.join("");
+  };
+}});
+
