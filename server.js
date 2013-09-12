109215c23622bc246908d280b73103a37fb1c944
Joseph Silvestre <joseph.silvestre38@gmail.com>
2013-09-12 17:08:40 +0200

    Added: step 1 code

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 var http = require('http'),
     express = require('express'),
     app = express();
+
+// At the root of your website, we show the index.html page
+app.get('/', function(req, res) {
+    res.sendfile('./public/index.html')
+});
+
+/* This will allow Cozy to run your app smoothly but
+ it won't break other execution environment */
+var port = process.env.PORT || 9250;
+var host = process.env.HOST || "127.0.0.1";
+
+// Starts the server itself
+var server = http.createServer(app).listen(port, host, function() {
+    console.log("Server listening to %s:%d within %s environment",
+                host, port, app.get('env'));
+});
