0716fc412ed6738ef3b1216452fa1992bf5cb76f
Joseph Silvestre <joseph.silvestre38@gmail.com>
2013-09-16 14:54:30 +0200

    Added: add and remove features

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ var http = require('http'),
 app.configure(function() {
     app.set('views', __dirname + '/public');
     app.engine('.html', require('jade').__express);
+
+    // Allows express to get data from POST requests
+    app.use(express.bodyParser());
 });
 
 // Let's define some bookmarks
@@ -23,6 +26,18 @@ app.get('/', function(req, res) {
     res.render('index.jade', params, function(err, html) {
         res.send(200, html);
     });
+});
+
+// We define a new route that will handle bookmark creation
+app.post('/add', function(req, res) {
+    bookmarks.push(req.body);
+    res.redirect('/');
+});
+
+// We define another route that will handle bookmark deletion
+app.get('/delete/:id', function(req, res) {
+    bookmarks.splice(req.params.id, 1);
+    res.redirect('/');
 });
 
 /* This will allow Cozy to run your app smoothly but
