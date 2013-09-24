4f89c5004a5bcaa080ca0c3aa10570cc04574756
Joseph Silvestre <joseph.silvestre38@gmail.com>
2013-09-24 15:18:36 +0200

    Added: first sqlite requests

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http'),
     express = require('express'),
-    app = express();
+    app = express(),
+    sqlite3 = require('sqlite3').verbose(),
+    db = new sqlite3.Database('cozy');
 
 /* We add configure directive to tell express to use Jade to
    render templates */
@@ -12,6 +14,29 @@ app.configure(function() {
     app.use(express.bodyParser());
 });
 
+// Database initialization
+db.serialize(function() {
+    db.get("SELECT name FROM sqlite_master WHERE type='table' AND name='bookmarks'", function(err, row) {
+        if(err !== null) {
+            console.log(err);
+        }
+        else if(row == null) {
+            db.run('CREATE TABLE "bookmarks" ("title" VARCHAR(255), url VARCHAR(255))', function(err) {
+                if(err !== null) {
+                    console.log(err);
+                }
+                else {
+                    console.log("SQL Table 'bookmarks' initialized.");
+                }
+            });
+        }
+        else {
+            console.log("SQL Table 'bookmarks' already initialized.");
+            //db.run("DROP TABLE 'bookmarks'");
+        }
+    });
+});
+
 // Let's define some bookmarks
 var bookmarks = []
 bookmarks.push({title: "Cozycloud", url: "https://cozycloud.cc"});
@@ -20,18 +45,34 @@ bookmarks.push({title: "My Cozy", url: "http://localhost:9104/"});
 
 // We render the templates with the data
 app.get('/', function(req, res) {
-    params = {
-        "bookmarks": bookmarks
-    }
-    res.render('index.jade', params, function(err, html) {
-        res.send(200, html);
+
+    db.all('SELECT * FROM bookmarks ORDER BY title', function(err, row) {
+        if(err !== null) {
+            res.send(500, "An error has occurred -- " + err);
+        }
+        else {
+            params = {
+                "bookmarks": row
+            }
+            res.render('index.jade', params, function(err, html) {
+                res.send(200, html);
+            });
+        }
     });
+
 });
 
 // We define a new route that will handle bookmark creation
 app.post('/add', function(req, res) {
-    bookmarks.push(req.body);
-    res.redirect('/');
+    //bookmarks.push(req.body);
+    db.run("INSERT INTO 'bookmarks' VALUES('" + body.title + "', '" + body.url + "')", function(err) {
+        if(err !== null) {
+            res.send(500, "An error has occurred -- " + err);
+        }
+        else {
+            res.redirect('/');
+        }
+    });
 });
 
 // We define another route that will handle bookmark deletion
